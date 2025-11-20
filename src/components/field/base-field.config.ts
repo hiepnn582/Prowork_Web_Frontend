08bf3fa29e3725a43aed77074eaa7183b3b4b3e5
hiepnn582 <nnhiep582002@gmail.com>
2025-11-20 15:35:02 +0700

    feat(register): custom element-plus color, selector style, create base form

--- src/components/field/base-field.config.ts
+++ src/components/field/base-field.config.ts
@@ -1,7 +1,12 @@
+import type { VNode } from 'vue';
 import type { FormItemProps } from 'element-plus';
 import { EFieldType } from '@/constants/index.constants';
 import type { TInputConfig } from '../input/base-input.config';
 import type { TButtonConfig } from '../button/base-button.config';
+
+type TSlotContent = (slotProps?: Record<string, unknown>) => VNode | VNode[];
+
+type TonClick = () => void;
 
 type TFormItemConfig = Partial<FormItemProps> & TCustomFieldConfig;
 
@@ -16,6 +21,8 @@ interface IFieldProps {
   fieldConfig: TFieldConfig;
   fieldType: EFieldType;
   fieldName: string;
+  content?: TSlotContent;
+  onClick?: TonClick;
 }
 
 const DEFAULT_CONFIG: TFormItemConfig = {};
---
+++ src/components/form/BaseForm.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+//#region Imports
+import { computed } from 'vue';
+import { ElForm } from 'element-plus';
+import { type TFormConfig, type IFormProps, DEFAULT_CONFIG } from './base-form.config';
+import BaseField from '../field/BaseField.vue';
+//#endregion
+
+//#region Props & Emits
+const props = defineProps<IFormProps>();
+
+const emit = defineEmits<{
+  onClick: [];
+}>();
+//#endregion
+
+//#region Composables
+
+//#endregion
+
+//#region Reactive state
+
+//#endregion
+
+//#region Computed
+const mergedConfig = computed<TFormConfig>(() =>
+  props.config ? { ...DEFAULT_CONFIG, ...props.config } : { ...DEFAULT_CONFIG },
+);
+//#endregion
+
+//#region Methods
+const handleClickField = () => {
+  emit('onClick');
+};
+//#endregion
+
+//#region Lifecycle hooks
+
+//#endregion
+
+//#region Watch/WatchEffect
+
+//#endregion
+
+//#region Expose
+
+//#endregion
+</script>
+
+<template>
+  <el-form v-bind="mergedConfig">
+    <BaseField
+      v-for="(fieldProp, index) in mergedConfig.fieldPropsList"
+      :key="index"
+      :fieldName="fieldProp.fieldName"
+      :fieldType="fieldProp.fieldType"
+      :fieldConfig="fieldProp.fieldConfig"
+    >
+      <component v-if="fieldProp.content" :is="fieldProp.content()" @onClick="handleClickField" />
+    </BaseField>
+  </el-form>
+</template>
