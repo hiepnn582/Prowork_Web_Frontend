de77ab0ca318831a20976233434001fab1f1ba9e
hiepnn582 <nnhiep582002@gmail.com>
2025-12-05 00:35:43 +0700

    feat(register): install lodash, handle response register api

--- src/constants/index.constants.ts
+++ src/constants/index.constants.ts
@@ -3,3 +3,5 @@ export { EFieldType, fieldComponentMapper } from './field.constants';
 export { VALIDATION_NUMBER, VALIDATION_REGEX, validationMessage } from './validate.constants';
 export { APP_CODE } from './code.constants';
 export { ToastType } from './toast.constants';
+export { RoleType, Gender } from './user.constants';
+export { RouteName, RoutePath } from './router.constants';
---
+++ src/constants/router.constants.ts
@@ -0,0 +1,9 @@
+export const RouteName = {
+  Register: 'register',
+  Home: 'home',
+};
+
+export const RoutePath = {
+  Register: '/register',
+  Home: '/home',
+};
