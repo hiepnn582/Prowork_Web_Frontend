de5f2dbed95504ffc7919f83bff6f044d68315d2
hiepnn582 <nnhiep582002@gmail.com>
2025-12-04 22:36:37 +0700

    feat(register): add loading service + base api

---
+++ src/stores/loading.stores.ts
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+import { ElLoading, type LoadingInstance } from 'element-plus';
+
+export const useLoadingStore = defineStore('loading', () => {
+  let loadingInstance: LoadingInstance;
+
+  const start = () => {
+    loadingInstance = ElLoading.service({ lock: true, background: 'rgba(0, 0, 0, 0.4)' });
+    loadingInstance.visible.value = true;
+  };
+
+  const stop = () => {
+    if (loadingInstance) {
+      loadingInstance.close();
+    }
+  };
+
+  return { start, stop };
+});
